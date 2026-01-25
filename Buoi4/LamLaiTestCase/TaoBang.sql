bca51a12beaee3ee8a7f9378ff05d85d6b5170dd
dauxanhduamuoi <dauxanhduamuoiabc123@gmail.com>
2026-01-25 22:50:36 +0700

    Add files via upload

---
+++ Buoi4/LamLaiTestCase/TaoBang.sql
@@ -0,0 +1,17 @@
+﻿-- Tạo database
+CREATE DATABASE OrganizationDB;
+GO
+
+USE OrganizationDB;
+GO
+
+-- Tạo bảng Organization
+CREATE TABLE Organization (
+    OrgId INT IDENTITY(1,1) PRIMARY KEY,
+    OrgName NVARCHAR(255) NOT NULL,
+    Address NVARCHAR(255) NOT NULL,
+    Phone VARCHAR(20) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    CreatedDate DATETIME NOT NULL DEFAULT GETDATE()
+);
+SELECT * FROM Organization;
